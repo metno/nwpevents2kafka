fdf8ec6156f1e384129204db971db800d685de46
Espen Myrland <espenm@met.no>
2016-09-05 12:24:48 +0200

    ssh -T hangs/is slow. Remove the -T option.

--- sendevent.sh
+++ sendevent.sh
@@ -1,3 +1,4 @@
 ./send_event -c 06 -n start_model -d arome2_5_arctic -s espenm@pc4496
 ./send_event  -n ping -d ecflow -s pc4496  -k dill=dall,host=vilje
 ./send_event -c 06 -n model_started -d meps -s espenm@pc4496 -k mode=prod,host=vilje
+./send_event -c 06 -n model_started -d meps -s espenm@pc4496 -k mode=prod,host=frost
